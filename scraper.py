28246a73a23aad720d70998e1e632ab3d5c106d4
mashmac2 <mikeshenry@gmail.com>
2014-12-30 22:10:46 +0200

    trying again. sqlite error

--- scraper.py
+++ scraper.py
@@ -10,12 +10,12 @@ html = scraperwiki.scrape("http://steamcommunity.com/id/mikeshenry/")
 #
 # # Find something on the page using css selectors
 root = lxml.html.fromstring(html)
-content = root.cssselect("div#recentgame_quicklinks h2")
+content = root.cssselect("div#recentgame_quicklinks")
 
 date = datetime.date.today()
 #
 # # Write out to the sqlite database using scraperwiki library
-scraperwiki.sqlite.save(unique_keys=date, data={"content": content, "date": date})
+scraperwiki.sqlite.save(unique_keys=['date'], data={"content": content, "date": date})
 #
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
